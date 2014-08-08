49fc25a23b3afc8451b2da11291968f499c39f69
danyalzia <catofdanyal@yahoo.com>
2014-08-08 19:24:50 +0500

    Implement xmlwriter

--- src/dirrlicht/io/xmlwriter.d
+++ src/dirrlicht/io/xmlwriter.d
@@ -26,7 +26,71 @@
 
 module dirrlicht.io.xmlwriter;
 
+import dirrlicht.core.array;
+import std.conv : to;
+import std.utf : toUTFz;
+
+/***
+ * Interface providing methods for making it easier to write XML files.
+ * This XML Writer writes xml files using in the platform dependent
+ * wchar_t format and sets the xml-encoding correspondingly.
+ */
 interface XMLWriter {
+	/***
+	 * Writes an xml 1.0 header.
+	 * Looks like &lt;?xml version="1.0"?&gt;. This should always
+	 * be called before writing anything other, because also the text
+	 * file header for unicode texts is written out with this method.
+	 */
+	void writeXMLHeader();
+
+	/***
+	 * Writes an xml element with maximal 5 attributes like "<foo />" or
+	 *  &lt;foo optAttr="value" /&gt;.
+	 *  The element can be empty or not.
+	 * Params:
+	 *  name = Name of the element
+	 * 	empty = Specifies if the element should be empty. Like
+	 * 		"<foo />". If You set this to false, something like this is
+	 * 		written instead: "<foo>".
+	 *  attr1Name = 1st attributes name
+	 *  attr1Value = 1st attributes value
+	 *  attr2Name = 2nd attributes name
+	 *  attr2Value = 2nd attributes value
+	 *  attr3Name = 3rd attributes name
+	 *  attr3Value = 3rd attributes value
+	 *  attr4Name = 4th attributes name
+	 *  attr4Value = 4th attributes value
+	 *  attr5Name = 5th attributes name
+	 *  attr5Value = 5th attributes value
+	 */
+	void writeElement(string name, bool empty=false,
+			string attr1Name = null, string attr1Value = null,
+			string attr2Name = null, string attr2Value = null,
+			string attr3Name = null, string attr3Value = null,
+			string attr4Name = null, string attr4Value = null,
+			string attr5Name = null, string attr5Value = null);
+
+	/// Writes an xml element with any number of attributes
+	void writeElement(string name, bool empty,
+				string[] names, string[] values);
+
+	/// Writes a comment into the xml file
+	void writeComment(string comment);
+
+	/// Writes the closing tag for an element. Like "</foo>"
+	void writeClosingTag(string name);
+
+	/***
+	 * Writes a text into the file.
+	 * All occurrences of special characters such as
+	 * & (&amp;), < (&lt;), > (&gt;), and " (&quot;) are automaticly
+	 * replaced.
+	 */
+	void writeText(string text);
+
+	/// Writes a line break
+	void writeLineBreak();
 	@property void* c_ptr();
 	@property void c_ptr(void* ptr);
 }
@@ -40,6 +104,42 @@ class CXMLWriter : XMLWriter {
     	this.ptr = ptr;
     }
 
+	void writeXMLHeader() {
+		irr_IXMLWriter_writeXMLHeader(ptr);
+	}
+	
+	void writeElement(string name, bool empty=false,
+			string attr1Name = null, string attr1Value = null,
+			string attr2Name = null, string attr2Value = null,
+			string attr3Name = null, string attr3Value = null,
+			string attr4Name = null, string attr4Value = null,
+			string attr5Name = null, string attr5Value = null) {
+		irr_IXMLWriter_writeElement(ptr, name.toUTFz!(const(dchar)*), empty, attr1Name.toUTFz!(const(dchar)*), attr1Value.toUTFz!(const(dchar)*), attr2Name.toUTFz!(const(dchar)*), attr2Value.toUTFz!(const(dchar)*), attr3Name.toUTFz!(const(dchar)*), attr3Value.toUTFz!(const(dchar)*), attr4Name.toUTFz!(const(dchar)*), attr4Value.toUTFz!(const(dchar)*), attr5Name.toUTFz!(const(dchar)*), attr5Value.toUTFz!(const(dchar)*));
+	}
+			
+	void writeElement(string name, bool empty, string[] names, string[] values) {
+		irr_array tempnames, tempvalues;
+		tempnames.data = names.ptr;
+		tempvalues.data = values.ptr;
+		irr_IXMLWriter_writeElement2(ptr, name.toUTFz!(const(dchar)*), empty, &tempnames, &tempvalues);
+	}
+	
+	void writeComment(string comment) {
+		irr_IXMLWriter_writeComment(ptr, comment.toUTFz!(const(dchar)*));
+	}
+	
+	void writeClosingTag(string name) {
+		irr_IXMLWriter_writeClosingTag(ptr, name.toUTFz!(const(dchar)*));
+	}
+	
+	void writeText(string text) {
+		irr_IXMLWriter_writeText(ptr, text.toUTFz!(const(dchar)*));
+	}
+	
+	void writeLineBreak() {
+		irr_IXMLWriter_writeLineBreak(ptr);
+	}
+	
     @property void* c_ptr() {
 		return ptr;
 	}
@@ -51,6 +151,25 @@ private:
     irr_IXMLWriter* ptr;
 }
 
+unittest {
+	import dirrlicht.compileconfig;
+	mixin(TestPrerequisite);
+}
+
 package extern(C):
 
 struct irr_IXMLWriter;
+
+void irr_IXMLWriter_writeXMLHeader(irr_IXMLWriter* writer);
+void irr_IXMLWriter_writeElement(irr_IXMLWriter* writer, const(dchar*) name, bool empty=false,
+		const(dchar*) attr1Name = null, const(dchar*) attr1Value = null,
+		const(dchar*) attr2Name = null, const(dchar*) attr2Value = null,
+		const(dchar*) attr3Name = null, const(dchar*) attr3Value = null,
+		const(dchar*) attr4Name = null, const(dchar*) attr4Value = null,
+		const(dchar*) attr5Name = null, const(dchar*) attr5Value = null);
+void irr_IXMLWriter_writeElement2(irr_IXMLWriter* writer, const(dchar*) name, bool empty,
+			irr_array* names, irr_array* values);
+void irr_IXMLWriter_writeComment(irr_IXMLWriter* writer, const(dchar*) comment);
+void irr_IXMLWriter_writeClosingTag(irr_IXMLWriter* writer, const(dchar*) name);
+void irr_IXMLWriter_writeText(irr_IXMLWriter* writer, const(dchar*) text);
+void irr_IXMLWriter_writeLineBreak(irr_IXMLWriter* writer);
